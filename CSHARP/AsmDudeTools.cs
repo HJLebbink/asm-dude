1ff5dc969ec157ea3bbf1790b96f7158cb838fe0
Henk-Jan Lebbink <henkjan.lebbink@gmail.com>
2016-03-23 23:13:24 +0100

    added method to write to output in the general window

--- CSHARP/AsmDudeTools.cs
+++ CSHARP/AsmDudeTools.cs
@@ -11,6 +11,8 @@ using System.Windows.Media.Imaging;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 
 namespace AsmDude {
 
@@ -48,6 +50,17 @@ namespace AsmDude {
                 Debug.WriteLine("WARNING: bitmapFromUri: could not read icon from uri " + bitmapUri.ToString() + "; " + e.Message);
             }
             return bitmap;
+        }
+
+        public static void Output(string msg) {
+            // Get the output window
+            var outputWindow = Package.GetGlobalService(typeof(SVsOutputWindow)) as IVsOutputWindow;
+            // Ensure that the desired pane is visible
+            var paneGuid = Microsoft.VisualStudio.VSConstants.OutputWindowPaneGuid.GeneralPane_guid;
+            IVsOutputWindowPane pane;
+            outputWindow.CreatePane(paneGuid, "General", 1, 0);
+            outputWindow.GetPane(paneGuid, out pane);
+            pane.OutputString(msg + Environment.NewLine);
         }
 
         public static bool isRemarkChar(char c) {
@@ -239,155 +252,149 @@ namespace AsmDude {
             }
         }
 
-        public static bool isRegister(string keyword) {
-
-            if (keyword.Length > 5) {
-                return false;
-            }
-
-            //TODO make a fast binary tree
+        private static bool isRegisterMethod1(string keyword) {
             //TODO  get this info from AsmDudeData.xml
             switch (keyword.ToUpper()) {
-                case "RAX":
-                case "EAX":
-                case "AX":
-                case "AL":
-                case "AH":
+                case "RAX"://
+                case "EAX"://
+                case "AX"://
+                case "AL"://
+                case "AH"://
 
-                case "RBX":
-                case "EBX":
-                case "BX":
-                case "BL":
-                case "BH":
+                case "RBX"://
+                case "EBX"://
+                case "BX"://
+                case "BL"://
+                case "BH"://
 
-                case "RCX":
-                case "ECX":
-                case "CX":
-                case "CL":
-                case "CH":
+                case "RCX"://
+                case "ECX"://
+                case "CX"://
+                case "CL"://
+                case "CH"://
 
-                case "RDX":
-                case "EDX":
-                case "DX":
-                case "DL":
-                case "DH":
+                case "RDX"://
+                case "EDX"://
+                case "DX"://
+                case "DL"://
+                case "DH"://
 
-                case "RSI":
-                case "ESI":
-                case "SI":
-                case "SIL":
+                case "RSI"://
+                case "ESI"://
+                case "SI"://
+                case "SIL"://
 
-                case "RDI":
-                case "EDI":
-                case "DI":
-                case "DIL":
+                case "RDI"://
+                case "EDI"://
+                case "DI"://
+                case "DIL"://
 
-                case "RBP":
-                case "EBP":
-                case "BP":
-                case "BPL":
+                case "RBP"://
+                case "EBP"://
+                case "BP"://
+                case "BPL"://
 
-                case "RSP":
-                case "ESP":
-                case "SP":
-                case "SPL":
+                case "RSP"://
+                case "ESP"://
+                case "SP"://
+                case "SPL"://
+                #region
 
-                case "R8":
-                case "R8D":
-                case "R8W":
-                case "R8B":
+                case "R8"://
+                case "R8D"://
+                case "R8W"://
+                case "R8B"://
 
-                case "R9":
-                case "R9D":
-                case "R9W":
-                case "R9B":
+                case "R9"://
+                case "R9D"://
+                case "R9W"://
+                case "R9B"://
 
-                case "R10":
-                case "R10D":
-                case "R10W":
-                case "R10B":
+                case "R10"://
+                case "R10D"://
+                case "R10W"://
+                case "R10B"://
 
-                case "R11":
-                case "R11D":
-                case "R11W":
-                case "R11B":
+                case "R11"://
+                case "R11D"://
+                case "R11W"://
+                case "R11B"://
 
-                case "R12":
-                case "R12D":
-                case "R12W":
-                case "R12B":
+                case "R12"://
+                case "R12D"://
+                case "R12W"://
+                case "R12B"://
 
-                case "R13":
-                case "R13D":
-                case "R13W":
-                case "R13B":
+                case "R13"://
+                case "R13D"://
+                case "R13W"://
+                case "R13B"://
 
-                case "R14":
-                case "R14D":
-                case "R14W":
-                case "R14B":
+                case "R14"://
+                case "R14D"://
+                case "R14W"://
+                case "R14B"://
 
-                case "R15":
-                case "R15D":
-                case "R15W":
-                case "R15B":
+                case "R15"://
+                case "R15D"://
+                case "R15W"://
+                case "R15B"://
+                case "MM0"://
+                case "MM1"://
+                case "MM2"://
+                case "MM3"://
+                case "MM4"://
+                case "MM5"://
+                case "MM6"://
+                case "MM7"://
 
-                case "MM0":
-                case "MM1":
-                case "MM2":
-                case "MM3":
-                case "MM4":
-                case "MM5":
-                case "MM6":
-                case "MM7":
+                case "XMM0"://
+                case "XMM1"://
+                case "XMM2"://
+                case "XMM3"://
+                case "XMM4"://
+                case "XMM5"://
+                case "XMM6"://
+                case "XMM7"://
 
-                case "XMM0":
-                case "XMM1":
-                case "XMM2":
-                case "XMM3":
-                case "XMM4":
-                case "XMM5":
-                case "XMM6":
-                case "XMM7":
+                case "XMM8"://
+                case "XMM9"://
+                case "XMM10"://
+                case "XMM11"://
+                case "XMM12"://
+                case "XMM13"://
+                case "XMM14"://
+                case "XMM15"://
 
-                case "XMM8":
-                case "XMM9":
-                case "XMM10":
-                case "XMM11":
-                case "XMM12":
-                case "XMM13":
-                case "XMM14":
-                case "XMM15":
+                case "YMM0"://
+                case "YMM1"://
+                case "YMM2"://
+                case "YMM3"://
+                case "YMM4"://
+                case "YMM5"://
+                case "YMM6"://
+                case "YMM7"://
 
-                case "YMM0":
-                case "YMM1":
-                case "YMM2":
-                case "YMM3":
-                case "YMM4":
-                case "YMM5":
-                case "YMM6":
-                case "YMM7":
+                case "YMM8"://
+                case "YMM9"://
+                case "YMM10"://
+                case "YMM11"://
+                case "YMM12"://
+                case "YMM13"://
+                case "YMM14"://
+                case "YMM15"://
 
-                case "YMM8":
-                case "YMM9":
-                case "YMM10":
-                case "YMM11":
-                case "YMM12":
-                case "YMM13":
-                case "YMM14":
-                case "YMM15":
+                case "ZMM0"://
+                case "ZMM1"://
+                case "ZMM2"://
+                case "ZMM3"://
+                case "ZMM4"://
+                case "ZMM5"://
+                case "ZMM6"://
+                case "ZMM7"://
 
-                case "ZMM0":
-                case "ZMM1":
-                case "ZMM2":
-                case "ZMM3":
-                case "ZMM4":
-                case "ZMM5":
-                case "ZMM6":
-                case "ZMM7":
-
-                case "ZMM8":
-                case "ZMM9":
+                case "ZMM8"://
+                case "ZMM9"://
                 case "ZMM10":
                 case "ZMM11":
                 case "ZMM12":
@@ -412,11 +419,185 @@ namespace AsmDude {
                 case "ZMM29":
                 case "ZMM30":
                 case "ZMM31":
-
+                    #endregion
                     return true;
                 default:
                     return false;
             }
+        }
+
+        private static bool isNumber(char c) {
+            switch (c) {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9': return true;
+                default: return false;
+            }
+        }
+
+        private static bool isRegisterMethod2(string keyword) {
+            int length = keyword.Length;
+            string str = keyword.ToUpper();
+            char c1 = str[0];
+            char c2 = str[1];
+            char c3 = (length > 2) ? str[2] : ' ';
+            char c4 = (length > 3) ? str[3] : ' ';
+            char c5 = (length > 4) ? str[4] : ' ';
+
+            switch (length) {
+                #region length2
+                case 2: 
+                    switch (c1) {
+                        case 'A': return (c2 == 'X') || (c2 == 'H') || (c2 == 'L');
+                        case 'B': return (c2 == 'X') || (c2 == 'H') || (c2 == 'L') || (c2 == 'P');
+                        case 'C': return (c2 == 'X') || (c2 == 'H') || (c2 == 'L');
+                        case 'D': return (c2 == 'X') || (c2 == 'H') || (c2 == 'L') || (c2 == 'I');
+                        case 'S': return (c2 == 'I') || (c2 == 'P');
+                        case 'R': return (c2 == '8') || (c2 == '9');
+                    }
+                    break;
+                #endregion
+                #region length3
+                case 3:
+                    switch (c1) {
+                        case 'R':
+                            switch (c2) {
+                                case 'A': return (c3 == 'X');
+                                case 'B': return (c3 == 'X') || (c3 == 'P');
+                                case 'C': return (c3 == 'X');
+                                case 'D': return (c3 == 'X') || (c3 == 'I');
+                                case 'S': return (c3 == 'I') || (c3 == 'P');
+                                case '8': return (c3 == 'D') || (c3 == 'W') || (c3 == 'B');
+                                case '9': return (c3 == 'D') || (c3 == 'W') || (c3 == 'B');
+                                case '1': return (c3 == '0') || (c3 == '1') || (c3 == '2') || (c3 == '3') || (c3 == '4');
+                            }
+                            break;
+                        case 'E':
+                            switch (c2) {
+                                case 'A': return (c3 == 'X');
+                                case 'B': return (c3 == 'X') || (c3 == 'P');
+                                case 'C': return (c3 == 'X');
+                                case 'D': return (c3 == 'X') || (c3 == 'I');
+                                case 'S': return (c3 == 'I') || (c3 == 'P');
+                            }
+                            break;
+                        case 'B': return (c2 == 'P') && (c3 == 'L');
+                        case 'S':
+                            switch (c2) {
+                                case 'P': return (c3 == 'L');
+                                case 'I': return (c3 == 'L');
+                            }
+                            break;
+                        case 'D': return (c2 == 'I') && (c3 == 'L');
+                        case 'M': 
+                            if (c2 == 'M') {
+                                switch (c3) {
+                                    case '0':
+                                    case '1':
+                                    case '2':
+                                    case '3':
+                                    case '4':
+                                    case '5':
+                                    case '6':
+                                    case '7': return true;
+                                }
+                            }
+                            break;
+                    }
+                    break;
+                #endregion
+                #region length4
+                case 4:
+                    switch (c1) {
+                        case 'R':
+                            if (c2 == '1') {
+                                switch (c3) {
+                                    case '0': return (c4 == 'D') || (c4 == 'W') || (c4 == 'B');
+                                    case '1': return (c4 == 'D') || (c4 == 'W') || (c4 == 'B');
+                                    case '2': return (c4 == 'D') || (c4 == 'W') || (c4 == 'B');
+                                    case '3': return (c4 == 'D') || (c4 == 'W') || (c4 == 'B');
+                                    case '4': return (c4 == 'D') || (c4 == 'W') || (c4 == 'B');
+                                    case '5': return (c4 == 'D') || (c4 == 'W') || (c4 == 'B');
+                                }
+                            }
+                            break;
+                        case 'X':
+                            if ((c2 == 'M') && (c3 == 'M')) {
+                                return isNumber(c4);
+                            }
+                            break;
+                        case 'Y':
+                            if ((c2 == 'M') && (c3 == 'M')) {
+                                return isNumber(c4);
+                            }
+                            break;
+                        case 'Z':
+                            if ((c2 == 'M') && (c3 == 'M')) {
+                                return isNumber(c4);
+                            }
+                            break;
+                    }
+                    break;
+                #endregion
+                #region length5
+                case 5:
+                    switch (c1) {
+                        case 'X':
+                            if ((c2 == 'M') && (c3 == 'M') && (c4 == '1')) {
+                                switch (c5) {
+                                    case '0': 
+                                    case '1': 
+                                    case '2': 
+                                    case '3': 
+                                    case '4': 
+                                    case '5': return true;
+                                }
+                            }
+                            break;
+                        case 'Y':
+                            if ((c2 == 'M') && (c3 == 'M') && (c4 == '1')) {
+                                switch (c5) {
+                                    case '0':
+                                    case '1':
+                                    case '2':
+                                    case '3':
+                                    case '4':
+                                    case '5': return true;
+                                }
+                            }
+                            break;
+                        case 'Z':
+                            if ((c2 == 'M') && (c3 == 'M')) {
+                                switch (c4) {
+                                    case '1': return isNumber(c5);
+                                    case '2': return isNumber(c5);
+                                    case '3': return ((c5 == '0') || (c5 == '1'));
+                                }
+                            }
+                            break;
+                    }
+                    break;
+                    #endregion
+            }
+            return false;
+        }
+        public static bool isRegister(string keyword) {
+            int length = keyword.Length;
+            if ((length > 5) || (length < 2)) {
+                return false;
+            }
+            bool b2 = isRegisterMethod2(keyword);
+#           if _DEBUG
+            if (b2 != isRegisterMethod1(keyword)) Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "INFO: isRegister; unequal responses"));
+#           endif
+            return b2;
         }
     }
 
@@ -502,6 +683,8 @@ namespace AsmDude {
         public IList<Tuple<string, string>> getLabels(ITextBuffer text) {
             var result = new List<Tuple<string, string>>();
 
+            //TODO: this can be slow on sources with many labels, better would be to precompute the list of labels.
+
             foreach (ITextSnapshotLine line in text.CurrentSnapshot.Lines) {
                 string str = line.GetText();
                 int strLength = str.Length;
@@ -514,13 +697,13 @@ namespace AsmDude {
                     if (c == ':') {
                         posColon = pos;
                         break;
-                    } else if ((c == ';') || (c == '#')) {
+                    } else if (AsmDudeToolsStatic.isRemarkChar(c)) {
                         break;
                     }
                 }
                 if (posColon > 0) {
                     string label = str.Substring(0, posColon).Trim();
-                    Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "INFO: {0}:getLabelsDictionary: label=\"{1}\"", this.ToString(), label));
+                    //Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "INFO: {0}:getLabelsDictionary: label=\"{1}\"", this.ToString(), label));
                     result.Add(new Tuple<string, string>(label, "line " + line.LineNumber + ": " +str.Substring(0, Math.Min(str.Length, 100))));
                 }
             }
@@ -570,7 +753,7 @@ namespace AsmDude {
             this._description = null;
         }
 
-        #region private stuff
+#region private stuff
 
         private void initData() {
             this._asmTypes = new Dictionary<string, AsmTokenTypes>();
@@ -685,6 +868,6 @@ namespace AsmDude {
             return this._xmlData;
         }
 
-        #endregion
+#endregion
     }
 }
