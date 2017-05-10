4e4706bb4c3748884e23b88b8b7dbd7820d234c1
Henk-Jan Lebbink <henkjan.lebbink@gmail.com>
2017-05-10 10:49:36 +0200

    ValueTuple version update

--- VS/CSHARP/asm-dude-vsix/Resources/examples/example_semantic_analysis.asm
+++ VS/CSHARP/asm-dude-vsix/Resources/examples/example_semantic_analysis.asm
@@ -1,28 +1,28 @@
 .intel_syntax noprefix
 
 	#region Semantic Error: usage of undefined register ax
-	mov cl, bl
-	xor cx, bx
-	bsf ax, cx
-	inc rax
+	;mov cl, bl
+	;xor cx, bx
+	;bsf ax, cx
+	;inc rax
 	#endregion
 
-	vaddpd xmm1, xmm2, xmm3 ; inimplemented instruction to stop the simulator
+	;vaddpd xmm1, xmm2, xmm3 ; inimplemented instruction to stop the simulator
 
 	#region Semantic Error: usage of undefined carry 
-	mov cl, 1
-	bsf ax, cx
+	;mov cl, 1
+	;bsf ax, cx
 	;rcl eax, 1
 	#endregion
 
-	vaddpd xmm1, xmm2, xmm3 ; inimplemented instruction to stop the simulator
+	;vaddpd xmm1, xmm2, xmm3 ; inimplemented instruction to stop the simulator
 	
 	#region move value to memory
 	;mov ptr qword [rax], 10
 	;mov rax, ptr qword [rax]
 	#endregion
 
-	vaddpd xmm1, xmm2, xmm3 ; inimplemented instruction to stop the simulator
+	;vaddpd xmm1, xmm2, xmm3 ; inimplemented instruction to stop the simulator
 
 	#region slow (expensive) instruction
 	;mov ptr qword [rax], 10
@@ -30,11 +30,19 @@
 	;popcnt rbx, rax
 	#endregion
 
+	vaddpd xmm1, xmm2, xmm3 ; inimplemented instruction to stop the simulator
+
+	#region moving undefined values to memory and retrieving it.
+	bsf ax, cx
+	mov ptr dword [rbx], eax
+	mov rcx, ptr qword [rbx]
+	#endregion
+
 	;vaddpd xmm1, xmm2, xmm3 ; inimplemented instruction to stop the simulator
 
 	#region Redundant instruction warning
-	mov rax, rbx
-	mov rbx, rax 
+	;mov rax, rbx
+	;mov rbx, rax 
 	#endregion
 
 
