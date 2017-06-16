7f8c98d8b8a6c37ab2059cba71e75440da1405e7
Henk-Jan Lebbink <henkjan.lebbink@gmail.com>
2017-06-16 15:15:06 +0200

    bumped version to 1.8.2.0

--- VS/CSHARP/asm-dude-vsix/source.extension.cs
+++ VS/CSHARP/asm-dude-vsix/source.extension.cs
@@ -9,9 +9,9 @@ namespace AsmDude
     {
         public const string Id = "c729d6b2-f412-49ed-893d-a8f61f25db98";
         public const string Name = "AsmDude";
-        public const string Description = "Syntax highlighting, code completion and code folding for assembly (.asm, .cod, .inc) source code.";
+        public const string Description = "Syntax highlighting, code completion and code folding for assembly (.asm, .cod, .inc, .s) source code.";
         public const string Language = "en-US";
-        public const string Version = "1.8.1.4";
+        public const string Version = "1.8.2.0";
         public const string Author = "Henk-Jan Lebbink";
         public const string Tags = "Assembly, Assembler, ASM, Syntax Highlighting, Code Completion, Folding, Label Analysis, x86_64, SSE, AVX, AVX2, AVX512";
     }
