f0b3d3868eb5021f8ef2571fc28d872342a5d312
Henk-Jan Lebbink <henkjan.lebbink@gmail.com>
2017-10-20 12:24:55 +0200

    minor updates

--- VS/CPP/ConsoleTest/ConsoleTest/Main.cpp
+++ VS/CPP/ConsoleTest/ConsoleTest/Main.cpp
@@ -2,6 +2,10 @@
 
 #include <iostream>
 
+// hit Local Windows Debugger to run this program, 
+// the breakpoint will hit,
+// Press Control + Alt + D to open the Disassembly window
+
 int main()
 {
 	std::cout << "Hello World!" << std::endl;
