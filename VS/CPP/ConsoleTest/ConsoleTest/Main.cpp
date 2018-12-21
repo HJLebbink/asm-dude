639467be90e6959c0b7f2c227a7daebf0e9d2d2f
Henk-Jan Lebbink <henkjan.lebbink@gmail.com>
2018-12-21 17:54:37 +0100

    added Disassembly-Window.png
    (cherry picked from commit 0186ebee415e3b711f57a37b6e5d98fce810c8a6)

--- VS/CPP/ConsoleTest/ConsoleTest/Main.cpp
+++ VS/CPP/ConsoleTest/ConsoleTest/Main.cpp
@@ -1,6 +1,7 @@
 // 'Hello World!' program 
 
 #include <iostream>
+#include <intrin.h>
 
 // hit Local Windows Debugger to run this program, 
 // the breakpoint will hit,
@@ -8,8 +9,15 @@
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+
+	__m512d zmm_a = _mm512_set1_pd(1.0);
+	for (int i = 0; i < (1 << 6); ++i) {
+		zmm_a = _mm512_div_pd(zmm_a, _mm512_set1_pd(i)); // search for vdivpd in the disassembly window
+	}
+
+	std::cout << "Hello world! " << zmm_a.m512d_f64[0] << std::endl;
 	std::cout << "Press any key to exit" << std::endl;
+
 	int c = getchar();
 	return 0;
 }
