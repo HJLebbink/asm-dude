27c68d268a3677d88ac0f68a1efde41f3ae1b8e3
Henk-Jan Lebbink <henkjan.lebbink@gmail.com>
2019-05-26 21:57:40 +0200

    Console cpp projects for windows and linux updated for testing syntax highlighting in disassembly window

--- VS/CPP/ConsoleLinuxApplicationTest/main.cpp
+++ VS/CPP/ConsoleLinuxApplicationTest/main.cpp
@@ -1,7 +1,8 @@
 // 'Hello World!' program 
 
 #include <iostream>
-#include <intrin.h>
+//#include <x86intrin.h>
+#include <cstdio>
 
 // 1] set a breakpoint at line 16.
 // 2] hit Local Windows Debugger to run this program, 
@@ -10,12 +11,12 @@
 
 int main()
 {
-	__m512d zmm_a = _mm512_set1_pd(1.0);
-	for (int i = 0; i < (1 << 6); ++i) {
-		zmm_a = _mm512_div_pd(zmm_a, _mm512_set1_pd(i)); // search for vdivpd in the disassembly window
-	}
+	//__m512d zmm_a = _mm512_set1_pd(1.0);
+	//for (int i = 0; i < (1 << 6); ++i) {
+	//	zmm_a = _mm512_div_pd(zmm_a, _mm512_set1_pd(i)); // search for vdivpd in the disassembly window
+	//}
 
-	std::cout << "Hello world! " << zmm_a.m512d_f64[0] << std::endl; // print the result such that it is not optimized away
+//	std::cout << "Hello world! " << zmm_a[0] << std::endl; // print the result such that it is not optimized away
 	std::cout << "Press any key to exit" << std::endl;
 
 	static_cast<void>(getchar());
